6baafae3c1cc30d5fe43c0025e431bc82b2ed32d
kdgarzon <karendanielagarzon11@gmail.com>
2025-04-16 16:36:30 -0500

    Se agregan variables de entorno y manejo de errores en controlador de usuario

--- src/controllers/controllerUser.js
+++ src/controllers/controllerUser.js
@@ -1,15 +1,15 @@
 const pool = require('../dbconexion')
 
-const getAllUsers = async (req, res) => {
+const getAllUsers = async (req, res, next) => {
     try {
         const allUsers = await pool.query("SELECT * FROM Usuarios")
         res.json(allUsers.rows)
     } catch (error) {
-        res.json({error: error.menssage});
+        next(error)
     }
 }
 
-const getUser = async (req, res) => {
+const getUser = async (req, res, next) => {
     try {
         const {id} = req.params
         const result = await pool.query("SELECT * FROM usuarios WHERE id_usuario = $1", [id])
@@ -19,11 +19,11 @@ const getUser = async (req, res) => {
         });
         res.json(result.rows[0]);
     } catch (error) {
-        res.json({error: error.menssage});
+        next(error)
     }
 }
 
-const createUser = async (req, res) => {
+const createUser = async (req, res, next) => {
     const {usuario, pass, id_rol} = req.body
     try {
         const result = await pool.query("INSERT INTO usuarios (usuario, pass, id_rol) VALUES ($1, $2, $3) RETURNING *", 
@@ -35,32 +35,40 @@ const createUser = async (req, res) => {
     
         res.json(result.rows[0])
     } catch (error) {
-        res.json({error: error.menssage});
+        next(error)
     }
 }
 
-const deleteUser = async (req, res) => {
+const deleteUser = async (req, res, next) => {
     const {idusuarioEliminar} = req.params
-    const result = await pool.query("DELETE FROM usuarios WHERE id_usuario = $1", [idusuarioEliminar])
+    try {
+        const result = await pool.query("DELETE FROM usuarios WHERE id_usuario = $1", [idusuarioEliminar])
     
-    if(result.rowCount === 0) return res.status(400).json({
-        message: "Usuario no se ha podido eliminar"
-    });
-    
-    return res.sendStatus(204);
+        if(result.rowCount === 0) return res.status(400).json({
+            message: "Usuario no se ha podido eliminar"
+        });
+        
+        return res.sendStatus(204);
+    } catch (error) {
+        next(error)
+    }
 }
 
-const updateUser = async (req, res) => {
+const updateUser = async (req, res, next) => {
     const {idusuarioActualizar} = req.params;
-    const {usuario, pass, id_rol} = req.body;
+    try {
+        const {usuario, pass, id_rol} = req.body;
     
-    const result = await pool.query("UPDATE usuarios SET usuario = $1, pass = $2, id_rol = $3 WHERE id_usuario = $4 RETURNING *", 
-        [usuario, pass, id_rol, idusuarioActualizar]);
+        const result = await pool.query("UPDATE usuarios SET usuario = $1, pass = $2, id_rol = $3 WHERE id_usuario = $4 RETURNING *", 
+            [usuario, pass, id_rol, idusuarioActualizar]);
 
-    if(result.rows.length === 0) return res.status(404).json({
-        message: "No es posible modificar el usuario seleccionado"
-    });
-    return res.json(result.rows[0])
+        if(result.rows.length === 0) return res.status(404).json({
+            message: "No es posible modificar el usuario seleccionado"
+        });
+        return res.json(result.rows[0])
+    } catch (error) {
+        next(error)
+    }
 }
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors')
 
 const horLabRoutes = require('./routes/horlab.routes'); //Se importa una función para mostrarla en el frontend
 
 const app = express();
+app.use(cors()); //Permite comunicaar ambos servidores BACKEND Y FRONTEND
 app.use(morgan('dev'));
 app.use(express.json());
 
 
 app.use(horLabRoutes);
+app.use((err, req, res, next) => {
+    return res.json({
+        message: err.message
+    })
+})
 
 app.listen(5000)
 console.log('Server on port 5000');
