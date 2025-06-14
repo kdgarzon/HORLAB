30c76402f983fcc2da9c3d2bdeeb1f0d723d50a7
kdgarzon <karendanielagarzon11@gmail.com>
2025-06-13 20:30:01 -0500

    Se agrega listado de salones por piso en el edificio techne

--- src/database/matrizHorarios.sql
+++ src/database/matrizHorarios.sql
@@ -319,3 +319,123 @@ UPDATE dia SET orden = 3 WHERE dia = 'MIERCOLES';
 UPDATE dia SET orden = 4 WHERE dia = 'JUEVES';
 UPDATE dia SET orden = 5 WHERE dia = 'VIERNES';
 UPDATE dia SET orden = 6 WHERE dia = 'SABADO';
+
+CREATE TABLE Pisos (
+    id_piso SERIAL,
+    nombre VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id_piso)
+);
+
+INSERT INTO Pisos (nombre) VALUES ('PISO 1');
+INSERT INTO Pisos (nombre) VALUES ('PISO 2');
+INSERT INTO Pisos (nombre) VALUES ('PISO 3');
+INSERT INTO Pisos (nombre) VALUES ('PISO 4');
+INSERT INTO Pisos (nombre) VALUES ('PISO 5');
+INSERT INTO Pisos (nombre) VALUES ('PISO 6');
+INSERT INTO Pisos (nombre) VALUES ('PISO 7');
+INSERT INTO Pisos (nombre) VALUES ('PISO 8');
+INSERT INTO Pisos (nombre) VALUES ('PISO 9');
+
+CREATE TABLE AulasPisos (
+    id_aula_piso SERIAL,
+    nombre_aula VARCHAR(150) NOT NULL,
+    id_piso INTEGER NOT NULL,
+    id_edificio INTEGER NOT NULL,
+    PRIMARY KEY (id_aula_piso),
+    FOREIGN KEY (id_piso) REFERENCES Pisos(id_piso) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (id_edificio) REFERENCES Edificio(id_edificio) ON UPDATE CASCADE ON DELETE CASCADE,
+);
+
+--PISO 1 EDIFICIO TECHNE (1)
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE PAVIMENTOS', 1, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE MATERIALES DE CONSTRUCCION Y PATOLOGIA', 1, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('CUARTO HUMEDO', 1, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('CUARTO DE MAQUINAS INSONORIZADO', 1, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE MOTORES DE COMBUSTION INTERNA', 1, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE TRIBOLOGIA', 1, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('TALLER DE CONTROL NUMERICO COMPUTARIZADO', 1, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE SUELOS', 1, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE SUELOS Y SERVICIOS', 1, 1);
+
+--PISO 2 EDIFICIO TECHNE (1)
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE MECANICA DE FLUIDOS Y MAQUINAS HIDRAULICAS', 2, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE DISEÑO Y DESARROLLO TECNOLOGICO / LABORATORIO DE PLASTICOS', 2, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('OFICINA DE LABORATORIOS Y TALLERES DE MECANICA', 2, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE AUTOMATIZACION Y CONTROL - NEUMATICA', 2, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE AUTOMATIZACION Y CONTROL - HIDRAULICA', 2, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE TRATAMIENTOS TERMICOS', 2, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE METALOGRAFIA', 2, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE CIENCIAS TERMICAS', 2, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE METROLOGIA', 2, 1);
+
+--PISO 3 EDIFICIO TECHNE (1)
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO APLICADO DE CIRCUITOS, ELECTRONICA Y CONTROL', 3, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO ESPECIALIZADO DE SISTEMAS ELECTRICOS', 3, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO APLICADO DE MAQUINAS ELECTRICAS', 3, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO ESPECIALIZADO DE SISTEMAS DE POTENCIA Y SMART GRID', 3, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO HAS', 3, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE DISEÑO DE PRODUCTO', 3, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO FMS', 3, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO GEIO', 3, 1);
+
+--PISO 4 EDIFICIO TECHNE (1)
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('SALA DE SOFTWARE DE INGENIERIA ELECTRICA', 4, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('SALA DE SOFTWARE DE INGENIERIA CIVIL', 4, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('SALA DE SOFTWARE DE MECANICA 1', 4, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('SALA DE SOFTWARE DE CIENCIAS BASICAS', 4, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('SALA DE SOFTWARE DE TECNOLOGIA E INGENIERIA DE PRODUCCION A', 4, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('SALA DE SOFTWARE DE TECNOLOGIA E INGENIERIA DE PRODUCCION B', 4, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('SALA DE SOFTWARE INGENIERIA ELECTRONICA 1', 4, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('SALA DE SOFTWARE INGENIERIA ELECTRONICA 2', 4, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('SALA DE SOFTWARE DE MECANICA 2', 4, 1);
+
+--PISO 5 EDIFICIO TECHNE (1)
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('COORDINACION DE LABORATORIOS Y ALMACEN', 5, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE REDES Y SEGURIDAD DE LA INFORMACION', 5, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE SISTEMAS DISTRIBUIDOS', 5, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE BASES DE DATOS AVANZADAS', 5, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE SIMULACION Y REALIDAD VIRTUAL', 5, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE INTELIGENCIA ARTIFICIAL', 5, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE INGENIERIA DE SOFTWARE Y COMPUTACION MOVIL', 5, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('SALA DE INFORMATICA 1', 5, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('SALA DE INFORMATICA 2', 5, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('SALA DE INFORMATICA 3', 5, 1);
+
+--PISO 6 EDIFICIO TECHNE (1)
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE CIRCUITOS IMPRESOS ATENCION A ESTUDIANTES', 6, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('TALLER MANTENIMIENTO ELECTRONICA', 6, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE ELECTRONICA ANALOGA', 6, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE TELECOMUNICACIONES', 6, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE AUTOMATIZACION Y CONTROL', 6, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE ELECTROMAGNETISMO /CIENCIAS BASICAS', 6, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE ELECTRONICA DIGITAL', 6, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE ELECTRONICA APLICADA', 6, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE PRACTICAS LIBRES', 6, 1);
+
+--PISO 7 EDIFICIO TECHNE (1)
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE FISICA MECANICA 1', 7, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE FISICA MECANICA 2', 7, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE FISICA MECANICA 3', 7, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE QUIMICA AMBIENTAL', 7, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO DE QUIMICA BASICA', 7, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO FLUIDOS Y TERMODINAMICA', 7, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('LABORATORIO OPTICA Y MODERNA', 7, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('ALMACEN DE EQUIPOS ESPECIALES', 7, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('ALMACEN DE REACTIVOS', 7, 1);
+
+--PISO 8 EDIFICIO TECHNE (1)
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('SALA DE AUDIOVISUALES 1', 8, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('SALA DE AUDIOVISUALES 2', 8, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('SALA DE AUDIOVISUALES 3', 8, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('SALA DE AUDIOVISUALES 4', 8, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('SALA DE AUDIOVISUALES 5', 8, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('SALA DE AUDIOVISUALES 6', 8, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('SALA DE AUDIOVISUALES 7', 8, 1);
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('SALA DE AUDIOVISUALES 8', 8, 1);
+
+--PISO 9 EDIFICIO TECHNE (1)
+INSERT INTO AulasPisos (nombre_aula, id_piso, id_edificio) VALUES ('CANCHA FUTBOL 5', 9, 1);
+
+
+
+
