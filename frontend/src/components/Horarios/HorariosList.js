5f704fdf9e4e08115181b46154bc72bb6232b659
kdgarzon <karendanielagarzon11@gmail.com>
2025-07-21 17:30:21 -0500

    Se ćrea nuevo archivo para el estilo de los botones con imagenes

--- frontend/src/components/Horarios/HorariosList.js
+++ frontend/src/components/Horarios/HorariosList.js
@@ -1,81 +1,12 @@
 import { useEffect, useState } from 'react';
-import { Box, Button, ButtonBase, Typography, Modal, } from '@mui/material';
+import { Box, Button, Typography, Modal, } from '@mui/material';
 import CsvUploader from './CsvUploader';
-import { styled } from '@mui/material/styles';
+import {ImageButton, ImageSrc, ImageBackdrop, Image, ImageMarked,} from '../Complementos/styleImagesButton';
 import { images } from '../Complementos/ArrayImagenes/HorariopictureList';
 import {style} from '../Complementos/stylesFiles';
 import { mostrarAlertaConfirmacion } from '../Alertas/Alert_Delete';
 import { alertaSuccessorError } from '../Alertas/Alert_Success';
 import { WithOptionalTooltip } from './DeshabilitarHorario';
-
-// Estilos para los botones con imagen
-const ImageButton = styled(ButtonBase)(({ theme, disabled }) => ({
-  position: 'relative',
-  height: 200,
-  width: '30%',
-  margin: 8,
-  opacity: disabled ? 0.4 : 1,
-  pointerEvents: disabled ? 'none' : 'auto',
-  [theme.breakpoints.down('sm')]: {
-    width: '100% !important',
-    height: 100,
-  },
-  '&:hover, &.Mui-focusVisible': {
-    zIndex: 1,
-    '& .MuiImageBackdrop-root': {
-      opacity: 0.15,
-    },
-    '& .MuiImageMarked-root': {
-      opacity: 0,
-    },
-    '& .MuiTypography-root': {
-      border: '4px solid currentColor',
-    },
-  },
-}));
-
-const ImageSrc = styled('span')({
-  position: 'absolute',
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0,
-  backgroundSize: 'cover',
-  backgroundPosition: 'center 40%',
-});
-
-const Image = styled('span')(({ theme }) => ({
-  position: 'absolute',
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0,
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  color: theme.palette.common.white,
-}));
-
-const ImageBackdrop = styled('span')(({ theme }) => ({
-  position: 'absolute',
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0,
-  backgroundColor: theme.palette.common.black,
-  opacity: 0.4,
-  transition: theme.transitions.create('opacity'),
-}));
-
-const ImageMarked = styled('span')(({ theme }) => ({
-  height: 3,
-  width: 18,
-  backgroundColor: theme.palette.common.white,
-  position: 'absolute',
-  bottom: -2,
-  left: 'calc(50% - 9px)',
-  transition: theme.transitions.create('opacity'),
-}));
 
 export default function HorariosList() {
   const [hasData, setHasData] = useState(false);
